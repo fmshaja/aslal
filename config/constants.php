6aec2f8267035288d2933740be5b365366ad1213
Aslal Shaja <61037944+fmshaja@users.noreply.github.com>
2026-09-10 13:19:22 +0530

    Update Google reCAPTCHA keys

--- config/constants.php
+++ config/constants.php
@@ -4,7 +4,7 @@
 define('SITE_URL', 'https://azlal.net'); // Change to local URL when developing
 
 // Google reCAPTCHA Keys (Test keys by default)
-define('RECAPTCHA_SITE_KEY', '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI');
-define('RECAPTCHA_SECRET_KEY', '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe');
+define('RECAPTCHA_SITE_KEY', '6Ld0trMtAAAAAEtgc848015N4m0SM1OvY2LJajxW');
+define('RECAPTCHA_SECRET_KEY', '6Ld0trMtAAAAADjBnrkq7FNsA8ivK32xCD28dO39');
 
 // Dynamic constants are now defined in functions.php based on database settings
